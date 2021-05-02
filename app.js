ff53e88a4f0de2d83b821eb8edd92203428eee03
HamzaCN <rajpoothamza923@gmail.com>
2021-05-02 21:08:32 +0500

    okay

--- app.js
+++ app.js
@@ -13,7 +13,17 @@ function addTodo(){
     delBtn.setAttribute("class", "deltbtn")
     delBtn.setAttribute("onclick", "deleteItem(this)")
     delBtn.appendChild(delText)
+    //create Edit Buton
+    var editBtn = document.createElement("button")
+    var editText = document.createTextNode("Edit")
+    editBtn.appendChild(editText)
+    editBtn.setAttribute("class", "deltbtn")
+    editBtn.setAttribute("onclick", "editItem(this)")
+
+
+
     li.appendChild(delBtn)
+    li.appendChild(editBtn)
 
     list.appendChild(li);
     todo_item.value = "";
@@ -22,4 +32,12 @@ function addTodo(){
 var list = document.getElementById("list")
 function deleteItem(e){
 e.parentNode.remove();
+}
+function editItem(e){
+    var val = prompt("Enter value",e.parentNode.firstChild.nodeValue)
+    e.parentNode.firstChild.nodeValue = val;
+}
+function deleteAll() {
+    alert("Deleted")
+    list.innerHTML = "";
 }
